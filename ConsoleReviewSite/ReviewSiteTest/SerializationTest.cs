4174ff2694f7490ec2eabb4ff4d3370c0e7eba27
Cameron Wagstaff <cameron.wagstaff.129@gmail.com>
2018-04-30 14:48:13 -0400

    Added test for serialization/deserialization.

--- ConsoleReviewSite/ReviewSiteTest/SerializationTest.cs
+++ ConsoleReviewSite/ReviewSiteTest/SerializationTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ReviewSiteData.Base.Model;
+using ReviewSiteLogic.Serialization;
 
 namespace ReviewSiteTest {
 
@@ -13,19 +15,41 @@ namespace ReviewSiteTest {
 
         [TestInitialize]
         public void Init() {
-            //todo use DummyDataLoader to bring in data
+            //todo use TestDataLoader to bring in data
+            var now = DateTime.Now;
+            restaurants = new List<Restaurant>();
+            reviews = new List<Review>();
+
+            reviews.Add(new Review() {
+                Body = "Body",
+                DatePublished = now,
+                Id = 3,
+                Name = "Testy",
+                Rating = 5,
+                RestaurantId = 1,
+                Title = "Title"
+            });
+            restaurants.Add(new Restaurant() {
+                Address = "123 Test LN",
+                Id = 1,
+                Name = "Tester",
+                Phone = "123456",
+                Reviews = new List<Review>(reviews.ToArray())
+            });
         }
 
         [TestMethod]
-        public void RestaurantSerialization() {
-            //Assert.Fail();
-        }
+        public void RestaurantWithReviewSerialization() {
+            var serialized = restaurants[0].Serialize();
 
-        [TestMethod]
-        public void ReviewSerialization() {
-            //Assert.Fail();
-        }
+            var result = RestaurantDeserializer.Deserialize(serialized);
 
+            Assert.AreEqual(restaurants[0].Address, result.Address);
+            Assert.AreEqual(restaurants[0].Id, result.Id);
+            Assert.AreEqual(restaurants[0].Name, result.Name);
+            Assert.AreEqual(restaurants[0].Phone, result.Phone);
+            Assert.AreEqual(restaurants[0].Reviews.ToString(), result.Reviews.ToString());
+        }
     }
 
 }
